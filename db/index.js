a694bf2aef00fda81f7c327fbe517ada850e0502
unoriginal1234 <eric.t.roth@gmail.com>
2024-04-29 10:12:40 -0700

    Connection string

--- db/index.js
+++ db/index.js
@@ -1,13 +1,24 @@
 require('dotenv').config()
 const {Client} = require('pg');
 
+// LOCAL
+// function clientConnect() {
+//   return new Client({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PW,
+//   })
+// };
+
+// DEPLOYED
+
+const connectionString = 'postgresql://postgres:uFCgmeyIdOGSXKclHqMXgDVDKcZcJSQo@monorail.proxy.rlwy.net:27781/railway';
+
 function clientConnect() {
   return new Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
+    connectionString
   })
 };
 
